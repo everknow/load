ad7eb1a9291620ff94ba01ed5f2ca4b5b423092d
Dominic Perini <dominic.perini@gmail.com>
2022-11-20 12:28:45 +0000

    moved to rc

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Load.MixProject do
   def project do
     [
       app: :load,
-      version: "0.1.0",
+      version: "0.1.0-rc.1",
       elixir: "~> 1.10",
       # build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
