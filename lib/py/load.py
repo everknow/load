62fb90434b22109e28887f016e00de7a95a0dcd2
Dominic Perini <dominic.perini@gmail.com>
2023-10-28 18:52:43 +0100

    send

--- lib/py/load.py
+++ lib/py/load.py
@@ -1,5 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+QUEUE=b'\x01'
+SID=QUEUE
+  
 def encode(data):
   return ((len(data)).to_bytes(2, byteorder='big')+data)
+
 def decode(data):
   l = int.from_bytes(data[:2],'big')
   return data[2:l+2]
+
+def send(data):
+  with os.fdopen(sys.stdout.fileno(), "wb", closefd=False) as stdout:
+    stdout.write(QUEUE+SID+encode(data))
+    stdout.flush()
